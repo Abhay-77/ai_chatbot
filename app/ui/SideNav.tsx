135386ce286b8335acc6364acd77840926fd0c39
Abhay Murali <abhaymuralim9@gmail.com>
2025-02-22 12:52:03 +0530

    error fixed

--- app/ui/SideNav.tsx
+++ app/ui/SideNav.tsx
@@ -1,12 +1,15 @@
 import { fetchFilteredHistory, historyItem } from "../lib/data";
 import SearchBar from "./SearchBar";
+import { Suspense } from "react";
 
 const SideNav = async () => {
   const historyItems: historyItem[] = await fetchFilteredHistory();
   return (
     <nav className="flex flex-col gap-5 h-screen">
       <div className="">
-        <SearchBar />
+        <Suspense>
+          <SearchBar />
+        </Suspense>
       </div>
       <div className="flex flex-col overflow-y-auto h-full ">
         {historyItems.map((item) => (
