cf30d80eb5ab5dde61d2544075bbda46d325510b
Abhay Murali <abhaymuralim9@gmail.com>
2025-03-05 19:46:16 +0530

    error fix

--- app/ui/PromptBar.tsx
+++ app/ui/PromptBar.tsx
@@ -3,15 +3,11 @@ import { Button } from "@/components/ui/button";
 import { FaArrowRight } from "react-icons/fa";
 import { ChangeEvent } from "react";
 
-const PromptBar = ({
-  setInput,
-  prompt,
-  onClick,
-}: {
-  setInput: any;
-  prompt: any;
-  onClick: any;
-}) => {
+const PromptBar: React.FC<{
+  setInput: (value: string) => void;
+  prompt: string;
+  onClick: () => void;
+}> = ({ setInput, prompt, onClick }) => {
   function handleChange(e: ChangeEvent<HTMLTextAreaElement>): void {
     setInput(e.target.value);
   }
@@ -20,22 +16,22 @@ const PromptBar = ({
     <>
       <div className="relative bg-gray-800 rounded-lg">
         {/* <form action=""> */}
-          <Textarea
-            className="border-none max-h-32 text-wrap w-[95%] text-gray-300 focus:outline-none focus:ring-0 focus-visible:ring-0"
-            placeholder="Type something here"
-            name="title"
-            value={prompt}
-            onChange={handleChange}
-          />
-          <Button
-            // type="submit"
-            className="absolute top-1/2 -translate-y-1/2 right-2 bg-inherit"
-            variant="outline"
-            size="icon"
-            onClick={onClick}
-          >
-            <FaArrowRight />
-          </Button>
+        <Textarea
+          className="border-none max-h-32 text-wrap w-[95%] text-gray-300 focus:outline-none focus:ring-0 focus-visible:ring-0"
+          placeholder="Type something here"
+          name="title"
+          value={prompt}
+          onChange={handleChange}
+        />
+        <Button
+          // type="submit"
+          className="absolute top-1/2 -translate-y-1/2 right-2 bg-inherit"
+          variant="outline"
+          size="icon"
+          onClick={onClick}
+        >
+          <FaArrowRight />
+        </Button>
         {/* </form> */}
       </div>
     </>
